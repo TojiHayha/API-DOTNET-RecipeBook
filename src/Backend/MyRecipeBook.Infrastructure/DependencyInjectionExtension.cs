b5c322a248720effe2f1260db73b9aa621801e1e
TojiHayha <fernando.aq502@gmail.com>
2025-02-27 00:47:21 -0300

    Feat: Adding Dependency Injection From Infrastructure.

---
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Infrastructure.DataAccess;
+using MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+namespace MyRecipeBook.Infrastructure;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+        AddDbContext_SqlServer(services);
+        AddRepositories(services);
+    }
+
+    private static void AddDbContext_SqlServer(IServiceCollection services)
+    {
+        var connectionString = "Data source=.;Initial Catalog=MyRecipeBook;User ID=sa;Password=MyRecipeBook3377;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+
+        services.AddDbContext<MyRecipeBookDbContext>(dbContextOptions =>
+        {
+            dbContextOptions.UseSqlServer(connectionString);
+        });
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+        services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+    }
+}
+
