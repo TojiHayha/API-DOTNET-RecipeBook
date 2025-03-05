8399e40a99b877719236512557592df636c19898
TojiHayha <fernando.aq502@gmail.com>
2025-03-05 01:16:36 -0300

    Feat: Adding VersionBase & User Table.

--- src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version001.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version001.cs
@@ -1,13 +1,17 @@
 ﻿using FluentMigrator;
+using MyRecipeBook.Infrastructure.Migrations.Versions.DbBase;
 
 namespace MyRecipeBook.Infrastructure.Migrations.Versions;
 
 [Migration(DatabaseVersions.TABLE_USER, "Create table to save the user's information")]
-public class Version001 : ForwardOnlyMigration
+public class Version001 : VersionBase
 {
     public override void Up()
     {
-        throw new NotImplementedException();
+        Create.Table("Users")
+            .WithColumn("Name").AsString(100).NotNullable()
+            .WithColumn("Email").AsString(100).NotNullable()
+            .WithColumn("Password").AsString(2000).NotNullable();
     }
 }
 
