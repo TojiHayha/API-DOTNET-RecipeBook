90dfbeeecd14c830dd763494296c9c725005a833
TojiHayha <fernando.aq502@gmail.com>
2025-03-04 21:56:19 -0300

    Feat: Create Database Schema (Dapper).

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace MyRecipeBook.Infrastructure.Migrations;
+public class DatabaseMigration
+{
+    public static void Migrate(string connectionString)
+    {
+        EnsureDatabaseCreated_SqlServer(connectionString);
+    }
+
+    private static void EnsureDatabaseCreated_SqlServer(string connectionString) 
+    {
+        var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+
+        var dataBaseName = connectionStringBuilder.InitialCatalog;
+
+        connectionStringBuilder.Remove("Database");
+
+        using var dbConnection = new SqlConnection(connectionStringBuilder.ConnectionString);
+
+        var parameters = new DynamicParameters();
+        parameters.Add("name", dataBaseName);
+
+        var records = dbConnection.Query("SELECT * FROM sys.databases WHERE name = @name", parameters);
+
+        if (records.Any() == false)
+            dbConnection.Execute($"CREATE DATABASE {dataBaseName}"); 
+    }
+}
+
