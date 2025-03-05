3b31db9cec20f695c458b35ece1d825357876267
TojiHayha <fernando.aq502@gmail.com>
2025-03-05 00:30:29 -0300

    Feat: Adding Fluent Migrator, DatabaseVersion & Versions.

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version001.cs
@@ -0,0 +1,13 @@
+﻿using FluentMigrator;
+
+namespace MyRecipeBook.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_USER, "Create table to save the user's information")]
+public class Version001 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        throw new NotImplementedException();
+    }
+}
+
