c3e2b34f00f91922fdf888f70569d53040e17de5
TojiHayha <fernando.aq502@gmail.com>
2025-03-09 17:56:13 -0300

    Feat: All User Validation Unit Tests.

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -9,11 +9,12 @@ public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
     public RegisterUserValidator()
     {
         RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
-
         RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.EMAIL_EMPTY);
-        RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_INVALID);
 
-        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_EMPTY);
+        When(user => string.IsNullOrEmpty(user.Email) == false, () => { 
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+        });
     }
 }
 
