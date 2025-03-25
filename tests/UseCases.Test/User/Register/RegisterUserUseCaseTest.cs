34b2c1ed7ef73f60cff0d8549761242ffa0a14c9
TojiHayha <fernando.aq502@gmail.com>
2025-03-24 22:43:17 -0300

    Feat: Create Test Use Case Exception.

--- tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -3,6 +3,8 @@ using CommonTestUtilities.Mapper;
 using CommonTestUtilities.Repositories;
 using CommonTestUtilities.Requests;
 using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
 using Shouldly;
 
 namespace UseCases.Test.User.Register;
@@ -22,15 +24,48 @@ public class RegisterUserUseCaseTest
         result.Name.ShouldBe(request.Name);
     }
 
-    private RegisterUserUseCase CreateUseCase()
+    [Fact]
+    public async Task Error_Email_Already_Registered()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var useCase = CreateUseCase(request.Email);
+
+        Func<Task> act = async () => await useCase.Execute(request);
+
+        var exception = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+        exception.ErrorMessages.Count.ShouldBe(1);
+        exception.ErrorMessages.First().ShouldBe(ResourceMessagesException.EMAIL_ALREADY_REGISTERED);
+    }
+
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var useCase = CreateUseCase();
+
+        Func<Task> act = async () => await useCase.Execute(request);
+
+        var exception = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+        exception.ErrorMessages.Count.ShouldBe(1);
+        exception.ErrorMessages.First().ShouldBe(ResourceMessagesException.NAME_EMPTY);
+    }
+
+    private RegisterUserUseCase CreateUseCase(string? email = null)
     {
         var mapper = MapperBuilder.Build();
         var passwordEncripter = PasswordEncripterBuilder.Build();
         var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
         var unitWork = UnitOfWorkBuilder.Build();
-        var readRepository = new UserReadOnlyRepositoryBuilder().Build();
+        var readRepositoryBuilder = new UserReadOnlyRepositoryBuilder();
 
-        return new RegisterUserUseCase(readRepository, writeRepository, unitWork, mapper, passwordEncripter);
+        if(string.IsNullOrEmpty(email) == false)
+        readRepositoryBuilder.ExistActiveUserWithEmail(email);
+
+        return new RegisterUserUseCase(readRepositoryBuilder.Build(), writeRepository, unitWork, mapper, passwordEncripter);
     }
-
 }
